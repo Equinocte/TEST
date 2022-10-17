965028e4d64c2b9339e71620a2e688df1049c5ec
Equinocte <emilien.maeso@e-artsup.net>
2022-10-17 15:13:28 +0100

    Trajet

---
+++ Railshooter/Assets/Scenes/Rail.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class Rail : MonoBehaviour
+{
+    private Transform[] nodes;
+
+    private void Start()
+    {
+        nodes = GetComponentsInChildren<Transform>();
+    }
+        private void OnDrawGizmos()
+    {
+        for (int i = 0; i < nodes.Length - 1; i++)
+        {
+            Handles.DrawDottedLine(nodes[i].position, nodes[i + 1].position, 3.0f);
+        }
+    }
+}
+
